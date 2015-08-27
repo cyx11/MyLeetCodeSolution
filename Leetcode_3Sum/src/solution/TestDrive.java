cea2f172b6567808f6ade185866aa3f688f4e0fc
Yuxuan Chen <chenyanchao6@126.com>
2015-08-26 23:35:33 -0400

    Add LeetCode3Sum
    Add LeetCode3Sum

---
+++ Leetcode_3Sum/src/solution/TestDrive.java
@@ -0,0 +1,12 @@
+package solution;
+
+public class TestDrive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution sol = new Solution();
+		int[] sums = {-1,0,1,2,-1,4};
+		System.out.println(sol.threeSum(sums));
+	}
+
+}
